c77104a62c85a56ae9fec35b12db428d0c1d4aaa
Sarvar <sarvarrejametov41@gmail.com>
2026-04-20 17:14:36 +0500

    emailga cod yubarish

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/sarvarrejametov41-cpu/email.git
-git push -u origin main
+import smtplib, ssl
+from dotenv import dotenv_values
+config = dotenv_values()
+email = config["EMAIL"]
+receiver_email = config["RECEIVER_EMAIL"]
+password =  config["PASSWORD"]
+
+def sent_message(mail):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "sarvarrejametov41@gmail.com"  # Enter your address
+    receiver_email = "sarvarrejametov41@gmail.com"  # Enter receiver address
+    password = "vztahzosufsgyuph"
+    message = f"""\
+    {mail}"""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+sent_message("Salom")
